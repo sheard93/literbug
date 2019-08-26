63b7d5fd795734b2145f8db3dd9a9f11300043a8
sheard93 <shelbyzaglin@gmail.com>
2019-08-26 18:09:09 +0000

    Added add quote form, save user data, and validating user data

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,2 +1,5 @@
 class Quote < ApplicationRecord
+  validates :saying, presence: true, length: { maximum: 300, minimum: 10 }
+  validates :author, presence: true, length: { maximum: 50, minimum: 3 }
+  validates :book, presence: true, length: { maximum: 100, minimum: 2 }
 end
